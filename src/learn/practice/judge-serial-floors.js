8d8ffd2c8238d05b5fb75315e7cbfebe8db1ba62
f-koki <fukushimakoki0108@gmail.com>
2022-02-03 13:54:46 +0900

    refactor: change variable names to be more readable

--- src/learn/practice/judge-serial-floors.js
+++ src/learn/practice/judge-serial-floors.js
@@ -5,41 +5,41 @@ serialFloors.forEach((str) => {
 });
 
 function bundleSerialFloors(floors) {
-  var first = 0; // 連続して空いている最初の階
-  var last = 0; // 連続して空いている最後の階
+  var serialFirst = 0; // 連続して空いている最初の階
+  var serialLast = 0; // 連続して空いている最後の階
 
   var currentFloor = floors[0]; // 現在の階
-  var serialFloors = []; // 連続して空いている階を ${first}-${last} フォーマットで格納する配列
+  var serialFloors = []; // 連続して空いている階を ${serialFirst}-${serialLast} フォーマットで格納する配列
 
   for (i = 0; i < floors.length; i++) {
     currentFloor = floors[i];
     nextFloor = floors[i + 1];
 
     if (!nextFloor) {
-      if (!first) break;
-      last = currentFloor;
+      if (!serialFirst) break;
+      serialLast = currentFloor;
     } else {
       var isNextFloorSerial = nextFloor - currentFloor == 1; // 次の階が連続しているか
       // 連続する最初の階がまだ見つかっていない場合
-      if (!first) {
+      if (!serialFirst) {
         if (isNextFloorSerial) {
-          first = currentFloor;
+          serialFirst = currentFloor;
         }
         continue;
       }
       // 連続する最初の階が見つかっている場合
       if (!isNextFloorSerial) {
-        last = currentFloor;
+        serialLast = currentFloor;
       }
     }
 
     // 連続する最初の階と最後の階が見つかっている場合
-    if (first && last) {
-      serialFloors.push(first + "-" + last);
+    if (serialFirst && serialLast) {
+      serialFloors.push(serialFirst + "-" + serialLast);
 
-      // 連続する最初の階と最後の階を初期化し、次の連続する階を探す
-      first = 0;
-      last = 0;
+      // 初期化し、次の連続する階を探す
+      serialFirst = 0;
+      serialLast = 0;
     }
   }
   return serialFloors;
